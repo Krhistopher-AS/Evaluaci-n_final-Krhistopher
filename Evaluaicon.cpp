ba7c703209c69c8d9d35d5154cc77f384109ef64
Krhistopher <krissiguenza@gmail.com>
2021-05-08 10:49:20 -0600

    Mejorado
    Mejorado

--- Evaluaicon.cpp
+++ Evaluaicon.cpp
@@ -5,35 +5,35 @@ using namespace std;
 
 int main()
 {
-    /*
-     * Escriba un programa que verifique si el número ingresado por el usuario es un número estrobogramático
-     * Un número estrobogramático es aquel que se ve exactamente igual cuando se gira 180 grados, es decir si se pusiera de cabeza se vería exactamente igual
-     * Ejemplo 1: 88 ---> Sí es un número estrobogramático
-     * Ejemplo 2: 69 ---> Sí es un número estrobogramático
-     * Ejemplo 3: 123 ---> No es un número estrobogramático
-     * Ejemplo 4: 668 ---> No es un número estrobogramático
-     * Ejemplo 5: 10801 ---> Sí es un número estrobogramático
-     * Clave 1: Si un número tiene al menos uno de estos dígitos: 2, 3, 4, 5 o 7, entonces no es un número estrobogramático
-     * Clave 2: No solo basta con tener los dígitos correctos, recuerde que al girarlo 180 grados se debe tener el mismo orden
-     */
-    cout << "Challenge #2" << endl;
-    int num;
-    cout << "Ingrese un numero:" << endl;
-    cin >> num;
-    // Comience a programar desde aquí
-   
-    if (esPrimo(num)) {
-       std::cout << "Es estrobogramático";
-     } else {
-    std::cout << "NO es estrobogramático";
-     }
-        }
-
-    bool esPrimo(int numero) {
-  
-  if (num == 0 || num == 1 || num == 4) return false;
-  for (int x = 2; x < num / 2; x++) {
-    if (num % x == 0) return false;
+  /*
+   * Escriba un programa que verifique si el número ingresado por el usuario es un número estrobogramático
+   * Un número estrobogramático es aquel que se ve exactamente igual cuando se gira 180 grados, es decir si se pusiera de cabeza se vería exactamente igual
+   * Ejemplo 1: 88 ---> Sí es un número estrobogramático
+   * Ejemplo 2: 69 ---> Sí es un número estrobogramático
+   * Ejemplo 3: 123 ---> No es un número estrobogramático
+   * Ejemplo 4: 668 ---> No es un número estrobogramático
+   * Ejemplo 5: 10801 ---> Sí es un número estrobogramático
+   * Clave 1: Si un número tiene al menos uno de estos dígitos: 2, 3, 4, 5 o 7, entonces no es un número estrobogramático
+   * Clave 2: No solo basta con tener los dígitos correctos, recuerde que al girarlo 180 grados se debe tener el mismo orden
+   */
+   cout << "Challenge #2" << endl;
+   string num;
+   string num_Al="";
+   cout << "Ingrese un numero:" << endl;
+   cin >> num;
+   // Comience a programar desde aquí 
+  for (int i = num.legth() - 1; i > -1 ; i--)
+  {
+    num_Al = num_Al +numero[i];
   }
-    return 0;
+  if(num_Al == num)
+  {
+    cout << "Sí es un número estrobogramático" << endl;
+  }
+  else
+  {
+    cout << "No es un número estrobogramático" << endl;
+  }
+  
+  return 0;
 }
